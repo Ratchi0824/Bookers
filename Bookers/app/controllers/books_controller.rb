acd32a2733bd94e5ecdac59abb779a95796b9a89
Ratchi0824 <ratchi0824@gmail.com>
2020-07-23 19:35:35 +0900

    first-commit

---
+++ Bookers/app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+	def top
+	end
+  def index
+  	@books = Book.all
+    @book = Book.new
+  end
+
+  def show
+  	@book =  Book.find(params[:id])
+  end
+
+  def new
+  	@books = Book.all
+  end
+
+  def create
+  	@book = Book.new(book_params)
+    if @book.save
+    flash[:notice] = "Book was successfully created."
+  	redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render("books/index")
+    end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+    else
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated"
+      redirect_to book_path
+    else
+      @books = Book.all
+      render("books/edit")
+    end
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
